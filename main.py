50579cf2370675e07da1f54271fee9bcdc352056
Trepository-Arepository <109420023+Tunip-Anip@users.noreply.github.com>
2025-08-08 12:30:14 +1000

    Coded graph

--- main.py
+++ main.py
@@ -1,3 +1,18 @@
 import pandas as pd
-Inflation = pd.read_csv('Data/Infaltion.csv')
-Inflation
+
+Inflation = pd.read_csv('Inflation.csv')
+print(Inflation)
+
+Inflation[['Quarter', 'Annual change (%)']]
+
+
+import matplotlib.pyplot as plt
+Inflation.plot(
+               kind='bar',
+               x='Quarter',
+               y='Annual change (%)',
+               color='blue',
+               alpha=0.1,
+               title='Inflation across the years in Australia'
+              )
+plt.show()

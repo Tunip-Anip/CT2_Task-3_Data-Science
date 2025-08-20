3b372f1c12d5160991d21fe122f768b4f45fcb29
Trepository-Arepository <109420023+Tunip-Anip@users.noreply.github.com>
2025-08-20 18:31:13 +1000

    F

--- data_module.py
+++ data_module.py
@@ -2,8 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.transforms as mtransforms
 import time
-
-
+#Imports the required modules for the graphs and datasets
 
 Inflation = pd.read_csv('Inflation.csv')
 Inflation[['Quarter', 'Annual change (%)']]
@@ -11,7 +10,7 @@ Inflation[['Quarter', 'Annual change (%)']]
 
 Crime = pd.read_csv('Crime_Rate.csv')
 Crime[['Years', 'Crime(%)/House']]
-
+#Sets the datasets as a variable
 
 def graphInflation():
     global Inflation
@@ -27,7 +26,7 @@ def graphInflation():
         xlabel= 'Date per Quarters',
                )
     plt.show()
-
+#Visuallises the graph when called, specify type of graph, axis and values.
 
 
 def graphCrime():
@@ -39,27 +38,70 @@ def graphCrime():
             color='blue',
             alpha=0.1,
             title='Percentage of Victimisation From Total Housholds in Australia',
+            ylabel= 'Crime(%)/House',
+            xlabel= 'Years'
             )
     plt.show()
+    #Visuallises the graph when called, specify type of graph, axis and values.
 def tableCrime():
     print(Crime)
 
 def tableInflation():
     print(Inflation)
-    
+    #Prints the dataset
 def filterDataCrime(query):
     num = query[:4]
+    #Takes the first four characters of the inputted number which is the year.
     numint = int(num) - 2013
+    #The inputed year minus 2013 is the row number
     specific_rows = Crime.iloc[numint,0:2]
+    #Sets the filtered row into a variable from the row and column
     time.sleep(0.5)
     print(specific_rows)
+    #Prints the variable
 
 def filterDataInflation(query):
     num = int(query[-2:])
+    #from the inputted number takes the last two integers (which is also the year)
     floor = (num*4) - 64    
-    print(floor)
+    #Gets the lower range of numbers  from the year as each year contains 4 quarters Tthe 2nd row is the lowest in 2016)
     roof = (num*4) - 60
-    print(roof)
-    
+    #Gets the highest number in the range as each year contains 4 quarters (The 5th row is the highest of 2016)
     specific_rows = Inflation.iloc[floor:roof,0:3]
+    #Sets the filtered row into a variable from the row and column
     print(specific_rows)
+     #Prints the variable
+def graphBoth():
+    global Inflation
+    global Crime
+    Crimex = Crime['Years']
+    Crimey = Crime['Crime(%)/House']
+    Inflationx = Inflation['Quarter']
+    Inflationy = Inflation['Annual change (%)']
+
+    fig=plt.figure()
+    Crime=fig.add_subplot(111, label="1")
+    Inflation=fig.add_subplot(111, label="2", frame_on=False)
+    
+
+    Crime.plot(Crimex,Crimey,color="C0")
+    Crime.set_xlabel("Years", color="C0")
+    Crime.set_ylabel("Crime(%)/House", color="C0")
+    Crime.tick_params(axis='x', colors="C0")
+    Crime.tick_params(axis='y', colors="C0")
+
+    Inflation.plot(Inflationx,Inflationy,color="C1")
+    Inflation.xaxis.tick_top()
+    Inflation.yaxis.tick_right()
+    Inflation.set_xlabel('Date per Quarters', color="C1") 
+    Inflation.set_ylabel('Annual Change (%)', color="C1")       
+    Inflation.xaxis.set_label_position('top') 
+    Inflation.yaxis.set_label_position('right') 
+    Inflation.tick_params(axis='x', colors="C1")
+    Inflation.tick_params(axis='y', colors="C1")
+
+    
+
+
+    plt.show()
+        
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from data_module import (graphInflation,graphCrime,tableCrime,tableInflation,filterDataCrime,filterDataInflation)
+from data_module import (graphInflation,graphCrime,tableCrime,tableInflation,filterDataCrime,filterDataInflation,graphBoth)
 import time
+#Imports the modules to run the graphs
 def main_menu():
-    time.sleep(1)
+    #Checks for access
     print("For educational purposes the key is: U+1F511")
+    #This is the unicode for the Key Emoji 🔑
     time.sleep(1)
-    permission = input("Do you have permission - What is your key?")
+    permission = input("Do you have permission - What is your key? ")
+    
     
     Value = True
     if permission ==  "U+1F511":
@@ -12,9 +15,11 @@ def main_menu():
         access = 1
     else:
         pass
+
         #Value = False
-        #access = 0
-    
+       # access = 0
+    #Checks access
+
     while Value == True:
         print("\n=== Data Viewer Interface ===")
         print("1. View Crime dataset")
@@ -25,12 +30,14 @@ def main_menu():
         time.sleep(0.1)
         print("4. View Inflation visualisation")
         time.sleep(0.1)
-        print("5. Filter Crime Dataset")
+        print("5. View Both visualisations")
         time.sleep(0.1)
-        print("6. Filter Inflation Dataset")
+        print("6. Filter Crime Dataset")
         time.sleep(0.1)
-        print("7. Exit")        
-
+        print("7. Filter Inflation Dataset")
+        time.sleep(0.1)
+        print("8. Exit")        
+    # Prints the options for the user
         time.sleep(1)
 
         choice = input("Select an option (1-6): ").strip()
@@ -46,21 +53,37 @@ def main_menu():
         elif choice == '4':
             graphInflation()
         elif choice == '5':
-            query = str(input("What year do you want to query, eg: 2013–14 - "))
-            filterDataCrime(query)
+            graphBoth()
         elif choice == '6':
-            query = str(input("What year do you want to query, eg: 16 for 2016 - "))
-            filterDataInflation(query)
+            try:
+                query = str(input("What year do you want to query, eg: 2013–14 - "))
+                #Enters the year as shown in the example
+                filterDataCrime(query)
+            except:
+                print("Invalid query")
+                #checks if the value can be inputted without error; if it can't then it tries again
         elif choice == '7':
+            try:
+                query = str(input("What year do you want to query, eg: 16 for 2016 - "))
+                #Enters the year as shown in the example
+                filterDataInflation(query)
+            except:
+                print("Invalid query")
+                #checks if the value can be inputted without error; if it can't then it tries again
+
+        elif choice == '8':
             Value = False
-    
+        # Checks which option the User picks and  runsthe respective function
         else:
             print("Invalid selection. Please choose a number between 1 and 6.")
+        # if the user inputs an incorrect character it retries
 
     if Value == False and access == 1:
         print("Program Ended, Conclusions made")
     elif Value == False and access == 0:
         print("No access")
-        
+    #If the user doesnt have access ends the program
 if __name__ == "__main__":
     main_menu()
+    #If the file is being run directly (e.g., python main.py), then __name__ is set to "__main__").
+    #If the file is being imported as a module (e.g., import main), then __name__ is set to "main" (the file name, without .py).
